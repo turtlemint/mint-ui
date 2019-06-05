ca0f5eee9d0bc67b555ad31942f09d3246c86a95
Kushal V. Mahajan <kushal.m@turtlemint.com>
2019-06-05 17:35:36 +0530

    formatting of config

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,32 +5,32 @@
 
     addDecorator(jsxDecorator);
     addDecorator(
-    withInfo({
-        styles: {
-        header: {
-            h1: {
-            marginRight: '20px',
-            fontSize: '25px',
-            display: 'inline',
+        withInfo({
+            styles: {
+            header: {
+                h1: {
+                marginRight: '20px',
+                fontSize: '25px',
+                display: 'inline',
+                },
+                body: {
+                paddingTop: 0,
+                paddingBottom: 0,
+                },
+                h2: {
+                display: 'inline',
+                color: '#999',
+                },
             },
-            body: {
-            paddingTop: 0,
-            paddingBottom: 0,
+            infoBody: {
+                backgroundColor: '#eee',
+                padding: '0px 5px',
+                lineHeight: '2',
             },
-            h2: {
-            display: 'inline',
-            color: '#999',
             },
-        },
-        infoBody: {
-            backgroundColor: '#eee',
-            padding: '0px 5px',
-            lineHeight: '2',
-        },
-        },
-        inline: true,
-        source: false,
-    })
+            inline: true,
+            source: false,
+        })
     );
     addDecorator(withKnobs);
 
