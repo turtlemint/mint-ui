8142d070894b9da5d7e512b3049cea2b903c129d
Kushal V. Mahajan <kushal.m@turtlemint.com>
2019-09-17 17:08:22 +0530

    DS-18 fix

--- packages/tm-components-mobile/App.tsx
+++ packages/tm-components-mobile/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello world</Text>
     </View>
   );
 }
