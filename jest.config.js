87b7d3201b845556b20c156102e192d43cb0e885
Kushal V. Mahajan <kushal.m@turtlemint.com>
2019-09-29 14:40:14 +0530

    DS-20 Jest setup

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	cacheDirectory: ".jest-cache",
+	coverageDirectory: ".jest-coverage",
+	coveragePathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"],
+	coverageReporters: ["html", "text"],
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100
+		}
+	},
+	testPathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/lib/"]
+};
---
+++ packages/web/src/button.spec.js
@@ -0,0 +1,12 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+
+import Button from "./button";
+
+describe("Button", () => {
+	test("renders correctly", () => {
+		const tree = renderer.create(<Button>Test </Button>).toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
