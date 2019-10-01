28a4194e32122080c3ecd11d2a8d87708410c0fc
Kushal V. Mahajan <kushal.m@turtlemint.com>
2019-10-01 14:57:23 +0530

    DS-23 completed button stories

--- packages/web/src/button.stories.tsx
+++ packages/web/src/button.stories.tsx
@@ -2,31 +2,144 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Button } from "./button";
 import { action } from "@storybook/addon-actions";
-
 // Storybook Markdown support - https://github.com/storybookjs/storybook/issues/6434
 // import ButtonReadme from "./README.md";
+import Row from "@turtlemint/tm-components-shared/src/components/row";
+import Col from "@turtlemint/tm-components-shared/src/components/col";
 
 export const actions = {
 	onOutlineClick: action("onOutlineClick"),
 	onPrimaryClick: action("onPrimaryClick")
 };
 const stories = storiesOf("Buttons", module);
-stories.add("Button", () => <Button btnType="primary">Primary Button</Button>, {
-	notes: { Introduction: "Notes", "Design notes": "adad" }
-});
+stories.add(
+	"Primary",
+	() => (
+		<Row>
+			<Col sm={3} md={3} lg={3}>
+				<Button>Primary Button</Button>
+			</Col>
+			<Col sm={3} md={3} lg={3}>
+				<Button btnType="primary" disabled>
+					Primary Disabled
+				</Button>
+			</Col>
+			<Col sm={3} md={3} lg={3}>
+				<Button btnType="primary" size="md">
+					Primary Large
+				</Button>
+			</Col>
+		</Row>
+	),
+	{
+		notes: { Introduction: "Notes", "Design notes": "Lorem ipsum" }
+	}
+);
 stories.add("Outlined", () => (
-	<Button
-		btnType="outlined"
-		onClick={() => {
-			console.log("fired");
-		}}
-		{...actions}
-	>
-		Outlined button
-	</Button>
+	<Row>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="outlined"
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Outlined button
+			</Button>
+		</Col>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="outlined"
+				disabled
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Outlined disabled
+			</Button>
+		</Col>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="outlined"
+				size="md"
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Outlined large
+			</Button>
+		</Col>
+	</Row>
 ));
-stories.add("Disabled", () => (
-	<Button btnType="outlined" disabled {...actions}>
-		Button Outlined Disabled
-	</Button>
+stories.add("Danger", () => (
+	<Row>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="danger"
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Danger button
+			</Button>
+		</Col>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="danger"
+				disabled
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Danger disabled
+			</Button>
+		</Col>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="danger"
+				size="md"
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Danger large
+			</Button>
+		</Col>
+	</Row>
+));
+stories.add("Link", () => (
+	<Row>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="link"
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Link button
+			</Button>
+		</Col>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="link"
+				disabled
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Link disabled
+			</Button>
+		</Col>
+		<Col sm={3} md={3} lg={3}>
+			<Button
+				btnType="link"
+				size="md"
+				onClick={() => {
+					console.log("fired");
+				}}
+			>
+				Link large
+			</Button>
+		</Col>
+	</Row>
 ));
