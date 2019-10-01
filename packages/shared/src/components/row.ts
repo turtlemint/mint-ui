512a19f265a6af0027d00907f989e27511e21d82
Kushal V. Mahajan <kushal.m@turtlemint.com>
2019-10-01 14:32:49 +0530

    DS-24 added row and column components and moved breakpoints to shared folder

---
+++ packages/shared/src/components/row.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+interface RowProps {
+	children: React.ReactNode;
+}
+const Row = styled.div<RowProps>`
+	margin-left: -15px;
+	margin-right: -15px;
+	&:before,
+	&:after {
+		display: table;
+		content: " ";
+	}
+	&:after {
+		clear: both;
+	}
+`;
+
+export default Row;
