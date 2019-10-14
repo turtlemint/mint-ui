39c253138d8e0a3165def71e778ee20d6593ac0f
Kushal V. Mahajan <kushal.m@turtlemint.com>
2019-10-14 13:37:12 +0530

    DS-29 added default export for size.ts and types for exported members

--- packages/core/src/size.ts
+++ packages/core/src/size.ts
@@ -1,2 +1,4 @@
-export const BASE_FONT_SIZE = "1em";
-export const EMPTY_FONT_SIZE = "1.1em";
+export const BASE_FONT_SIZE: string = "1em";
+export const EMPTY_FONT_SIZE: string = "1.1em";
+
+export default Size;
