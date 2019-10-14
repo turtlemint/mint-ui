add38f1a1f08101ea2ef4d73be4ae759fcb25533
Kushal V. Mahajan <kushal.m@turtlemint.com>
2019-10-14 15:21:46 +0530

    DS-29 exposed Empty component from components package

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -3,5 +3,6 @@ import { default as Checkbox } from "./src/checkbox";
 import { default as Input } from "./src/input";
 import { default as Row } from "./src/grid/row";
 import { default as Col } from "./src/grid/col";
+import { default as Empty } from "./src/empty";
 
-export { Button, Checkbox, Input, Row, Col };
+export { Button, Checkbox, Input, Row, Col, Empty };
