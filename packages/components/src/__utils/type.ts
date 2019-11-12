479256dc05a2a6e1a46ed678988ae5f5b79a47f6
Kushal <kvmhjn@gmail.com>
2019-11-12 18:22:54 +0530

    DS-5 removed test change

--- packages/components/src/__utils/type.ts
+++ packages/components/src/__utils/type.ts
@@ -6,5 +6,4 @@ export const tupleNum = <T extends number[]>(...args: T) => args;
 export interface IDict<T> {
 	[key: string]: T;
 }
-//test change
 export type CommonTypeTuple = string | boolean | number | undefined;
