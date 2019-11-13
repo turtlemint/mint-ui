4ab407047e81a75f4f9d88b60415eb031181c21e
Kushal <kvmhjn@gmail.com>
2019-11-13 14:29:09 +0530

    DS-5 Document title

--- packages/components/.storybook/addons.js
+++ packages/components/.storybook/addons.js
@@ -7,3 +7,4 @@ import '@storybook/addon-viewport/register';
 // import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-actions/register';
+import './title-addon';
