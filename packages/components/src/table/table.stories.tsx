77b37ce9cf9a637d5f33b5a0e879b558e9f06874
Kushal <kvmhjn@gmail.com>
2019-12-30 20:39:50 +0530

    custom sort functionalit added to table component

--- packages/components/src/table/table.stories.tsx
+++ packages/components/src/table/table.stories.tsx
@@ -23,17 +23,64 @@ const columns = [
 	{
 		title: "Name",
 		dataIndex: "name",
-		key: "name"
+		key: "name",
+		sortDirections: ["ascends", "descends"],
+		sorter: (a: any, b: any, sortOrder: "ascends" | "descends") => {
+			var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+			var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+			if (sortOrder === "ascends") {
+				if (nameA < nameB) {
+					return -1;
+				}
+				if (nameA > nameB) {
+					return 1;
+				}
+				return 0;
+			}
+			if (nameA < nameB) {
+				return 1;
+			}
+			if (nameA > nameB) {
+				return -1;
+			}
+			return 0;
+		}
 	},
 	{
 		title: "Age",
 		dataIndex: "age",
-		key: "age"
+		key: "age",
+		sorter: (a: any, b: any, sortOrder: "ascends" | "descends") => {
+			if (sortOrder === "ascends") {
+				return a.age - b.age;
+			}
+			return b.age - a.age;
+		}
 	},
 	{
 		title: "Address",
 		dataIndex: "address",
-		key: "address"
+		key: "address",
+		sorter: (a: any, b: any, sortOrder: "ascends" | "descends") => {
+			var nameA = a.address.toUpperCase(); // ignore upper and lowercase
+			var nameB = b.address.toUpperCase(); // ignore upper and lowercase
+			if (sortOrder === "ascends") {
+				if (nameA < nameB) {
+					return -1;
+				}
+				if (nameA > nameB) {
+					return 1;
+				}
+				return 0;
+			}
+			if (nameA < nameB) {
+				return 1;
+			}
+			if (nameA > nameB) {
+				return -1;
+			}
+			return 0;
+		}
 	}
 ];
 
--- packages/components/src/table/table.tsx
+++ packages/components/src/table/table.tsx
@@ -2,12 +2,19 @@ import * as React from "react";
 import styled from "styled-components";
 import COLORS from "../__utils/colors";
 import { GlobalStyles } from "../app";
+import Icon from "../icon";
+// import { tuple } from "../__utils/type";
+
+// const sortDirectionsTuple = tuple("ascends", "descends");
+// type sortDirections = typeof sortDirectionsTuple[number];
 
 type ColumnType = {
 	title: string;
 	dataIndex: string;
 	key: string;
 	render?: (data?: any) => React.ReactNode;
+	sorter?: (a: any, b: any, sortOrder: "ascends" | "descends") => any;
+	sortDirections?: any;
 };
 interface TableProps {
 	dataSource: any;
@@ -15,17 +22,62 @@ interface TableProps {
 }
 
 export const Table = ({ dataSource, columns }: TableProps) => {
+	const [data, setData] = React.useState(dataSource);
+	const handleSort = (
+		sorter: (a: any, b: any, sortOrder: "ascends" | "descends") => any,
+		sortOrder: "ascends" | "descends"
+	) => {
+		const sortedData = data.sort((a: any, b: any) =>
+			sorter(a, b, sortOrder)
+		);
+		setData([...sortedData]);
+	};
+
 	return (
 		<TableEl>
 			<thead>
 				<tr>
-					{columns.map(column => (
-						<Th key={column.key}>{column.title}</Th>
-					))}
+					{columns.map(column => {
+						const [sortOrder, setSortOrder] = React.useState<any>(
+							column.sortDirections
+								? column.sortDirections[0]
+								: "ascends"
+						);
+						return (
+							<Th
+								key={column.key}
+								sorter={column.sorter ? true : false}
+								onClick={() => {
+									handleSort(
+										column.sorter
+											? column.sorter
+											: function() {},
+										sortOrder
+									);
+									if (sortOrder === "ascends") {
+										setSortOrder("descends");
+									} else {
+										setSortOrder("ascends");
+									}
+								}}
+							>
+								<FlexWrap>
+									{column.sorter ? (
+										<Icon
+											name="sort"
+											size={18}
+											style={{ marginRight: "15px" }}
+										/>
+									) : null}
+									<span>{column.title}</span>
+								</FlexWrap>
+							</Th>
+						);
+					})}
 				</tr>
 			</thead>
 			<tbody>
-				{dataSource.map((item: any) => (
+				{data.map((item: any) => (
 					<tr key={item.key}>
 						{Object.keys(item)
 							.filter(item => item !== "key")
@@ -44,24 +96,32 @@ const TableEl = styled.table`
 	border-collapse: separate;
 	border-spacing: 0;
 `;
+const FlexWrap = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+`;
 
-const Th = styled.th`
+const Th = styled.th<{ sorter: boolean }>`
 	text-align: left;
 	padding: 16px 16px;
 	overflow-wrap: break-word;
 	font-weight: 500;
 	background: ${COLORS.BACKGROUND_GREY};
-	border-bottom: 1px solid #e8e8e8;
+	border-bottom: 1px solid ${COLORS.GREY4};
 	transition: background 0.3s ease;
 	&:first-child {
 		border-top-left-radius: 4px;
 	}
+	cursor: ${({ sorter }) => (sorter ? "pointer" : "none")};
 `;
+
 const Td = styled.td`
 	padding: 16px 16px;
 	overflow-wrap: break-word;
 	border: 0;
 	text-align: left;
+	border-bottom: 1px solid ${COLORS.GREY4};
 `;
 
 export default Table;
