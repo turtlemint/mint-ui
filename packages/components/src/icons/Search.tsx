ab231133c655774a6a84f0ba9fabd37dad58ff45
RishikeshVedpathak <rishikeshvedpathak@gmail.com>
2020-01-21 16:46:07 +0530

    Updated icons.

--- packages/components/src/icons/Search.tsx
+++ packages/components/src/icons/Search.tsx
@@ -1,7 +1,7 @@
 import React, { SVGProps } from "react";
 
 const SvgSearch = (props: SVGProps<SVGSVGElement>) => (
-  <svg width={24} height={24} {...props}>
+  <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
     <path d="M15.867 14.32h-.818l-.284-.282A6.657 6.657 0 0016.38 9.69a6.69 6.69 0 10-6.69 6.69 6.66 6.66 0 004.349-1.612l.284.282v.815L19.466 21 21 19.466l-5.133-5.146zm-6.177 0a4.63 4.63 0 110-9.262 4.631 4.631 0 010 9.262z" />
   </svg>
 );
