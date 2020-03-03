ad516576f7a20a675e5f955141b32befdc312d9e
Kushal <kvmhjn@gmail.com>
2020-03-03 13:41:30 +0530

    checkbox stories changed to csf format

--- packages/components/src/chip/chip.stories.tsx
+++ packages/components/src/chip/chip.stories.tsx
@@ -1,20 +1,19 @@
 import * as React from "react";
-import { storiesOf } from "@storybook/react";
 import Chip from "./index";
 
-storiesOf("Chip", module).add(
-	"default",
-	() => (
-		<>
-			<Chip text="November" style={{ marginRight: "15px" }} />
-			<Chip text="October" done={true} style={{ marginRight: "15px" }} />
-			<Chip text="This Quarter" style={{ marginRight: "15px" }} />
-			<Chip text="Last Quarter" />
-		</>
-	),
-	{
-		info: {
-			propTables: [Chip]
-		}
+export default {
+	title: "Chip",
+	component: Chip,
+	parameters: {
+		componentSubtitle: "default"
 	}
+};
+
+export const Default = () => (
+	<>
+		<Chip text="November" style={{ marginRight: "15px" }} />
+		<Chip text="October" done={true} style={{ marginRight: "15px" }} />
+		<Chip text="This Quarter" style={{ marginRight: "15px" }} />
+		<Chip text="Last Quarter" />
+	</>
 );
