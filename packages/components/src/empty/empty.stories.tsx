406edbb53de48effb82528a50c0de0f933fe6ecb
Kushal <kvmhjn@gmail.com>
2020-03-03 13:44:29 +0530

    empty stories changed to csf format

--- packages/components/src/empty/empty.stories.tsx
+++ packages/components/src/empty/empty.stories.tsx
@@ -1,22 +1,20 @@
 import * as React from "react";
-import { storiesOf } from "@storybook/react";
 import Empty from "./";
 import SimpleImage from "./simple";
 
-storiesOf("Empty", module)
-	.add("default", () => {
-		return (
-			<Empty description="Default description">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-				Assumenda, corrupti.
-			</Empty>
-		);
-	})
-	.add("simple", () => {
-		return (
-			<Empty image={<SimpleImage />} description="Simple description">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Id,
-				nisi.
-			</Empty>
-		);
-	});
+export default {
+	title: "Empty",
+	component: Empty
+};
+
+export const All = () => (
+	<>
+		<Empty description="Default description">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+			corrupti.
+		</Empty>
+		<Empty image={<SimpleImage />} description="Simple description">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nisi.
+		</Empty>
+	</>
+);
