034409bfa33b21efa9c6cf5fd27a5d55fa96fc90
Kushal <kvmhjn@gmail.com>
2020-03-03 13:43:32 +0530

    ccomposite stories changed to mdx format

--- packages/components/src/composite-ui/stories/composite-ui.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { CompositeUI } from "../index";
-import config from "./mock.json";
-import { TypeMap } from "./type-map";
-
-storiesOf("Composite UI", module).add("default", () => {
-	return <CompositeUI config={config} typeMap={TypeMap} />;
-});
