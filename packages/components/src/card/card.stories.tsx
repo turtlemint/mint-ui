f7cbc7472a35d94789678cbcb82192cd6c98100a
Kushal <kvmhjn@gmail.com>
2020-03-03 12:02:35 +0530

    updated card prop headerBackgroundType, migrated stories to CSF format

--- packages/components/src/card/card.stories.tsx
+++ packages/components/src/card/card.stories.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { storiesOf } from "@storybook/react";
 import Card from "./index";
 import styled from "styled-components";
 import COLORS from "../__utils/colors";
@@ -11,77 +10,89 @@ const GreyBackground = styled.div`
 const Para = styled.p`
 	margin: 0;
 `;
-storiesOf("Card", module)
-	.addParameters({
-		info: {
-			propTables: [Card]
-		}
-	})
-	.add("Basic card", () => (
-		<>
-			<Card
-				title="Default size card"
-				extra={<a href="#">More</a>}
-				style={{ width: 300 }}
-			>
-				<Para>Card content</Para>
-				<Para>Card content</Para>
-				<Para>Card content</Para>
-			</Card>
-			<div style={{ marginTop: "20px" }}></div>
-			<Card
-				size="small"
-				title="Small size card"
-				extra={<a href="#">More</a>}
-				style={{ width: 300 }}
-			>
-				<Para>Card content</Para>
-				<Para>Card content</Para>
-				<Para>Card content</Para>
-			</Card>
-		</>
-	))
-	.add("Card with hover box shadow", () => (
+export default {
+	title: "Card",
+	component: Card,
+	parameters: {
+		componentSubtitle: "Basic"
+	}
+};
+
+/**
+ * By default the `Card` will take full width of the grid column put in. To control the width use style prop.
+ */
+export const Basic = () => (
+	<Card style={{ width: 300 }}>
+		<Para>Card content</Para>
+		<Para>Card content</Para>
+		<Para>Card content</Para>
+	</Card>
+);
+
+export const CustomActions = () => (
+	<Card
+		title="Custom Actions card"
+		extra={<a href="#">More</a>}
+		style={{ width: 300 }}
+	>
+		<Para>Card content</Para>
+		<Para>Card content</Para>
+		<Para>Card content</Para>
+	</Card>
+);
+
+export const Size = () => (
+	<>
+		<Card title="Default size card" style={{ width: 300 }}>
+			<Para>Card content</Para>
+			<Para>Card content</Para>
+			<Para>Card content</Para>
+		</Card>
+		<div style={{ marginTop: "20px" }}></div>
+		<Card size="small" title="Small size card" style={{ width: 300 }}>
+			<Para>Card content</Para>
+			<Para>Card content</Para>
+			<Para>Card content</Para>
+		</Card>
+	</>
+);
+
+export const BoxShadow = () => (
+	<Card
+		size="small"
+		hoverable={true}
+		title="Hoverable Card"
+		style={{ width: 300 }}
+	>
+		<Para>Card content</Para>
+		<Para>Card content</Para>
+		<Para>Card content</Para>
+	</Card>
+);
+
+export const BorderlessCard = () => (
+	<GreyBackground>
 		<Card
 			size="small"
-			hoverable={true}
-			title="Hoverable Card"
+			bordered={false}
+			title="Borderless Card"
 			style={{ width: 300 }}
 		>
 			<Para>Card content</Para>
 			<Para>Card content</Para>
 			<Para>Card content</Para>
 		</Card>
-	))
-	.add("Borderless card", () => (
-		<GreyBackground>
-			<Card
-				size="small"
-				bordered={false}
-				title="Borderless Card"
-				style={{ width: 300 }}
-			>
-				<Para>Card content</Para>
-				<Para>Card content</Para>
-				<Para>Card content</Para>
-			</Card>
-		</GreyBackground>
-	))
-	.add("Simple card", () => (
-		<Card style={{ width: 300 }}>
-			<Para>Card content</Para>
-			<Para>Card content</Para>
-			<Para>Card content</Para>
-		</Card>
-	))
-	.add("Header background card", () => (
-		<Card
-			title="Card header background"
-			headerBackground={true}
-			style={{ width: 300 }}
-		>
-			<Para>Card content</Para>
-			<Para>Card content</Para>
-			<Para>Card content</Para>
-		</Card>
-	));
+	</GreyBackground>
+);
+
+export const HeaderBackground = () => (
+	<Card
+		title="Card header background"
+		headerBackground={COLORS.BACKGROUND_GREY}
+		style={{ width: 300 }}
+	>
+		<Para>Card content</Para>
+		<Para>Card content</Para>
+		<Para>Card content</Para>
+	</Card>
+);
