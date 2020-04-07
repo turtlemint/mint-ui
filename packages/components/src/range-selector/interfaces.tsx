afd30ede0b8461939be3ec0f6dd6edaefd8070c3
Kushal V. Mahajan <kushal.m@turtlemint.com>
2020-04-07 14:39:33 +0000

    Publish
     - @turtlemint/mint-ui@3.4.1
     - @turtlemint/dotcom-ui@0.0.2-alpha.0

--- packages/components/src/range-selector/interfaces.tsx
+++ packages/components/src/range-selector/interfaces.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export interface ITrackProps {
 	min: number;
 	max: number;
-	localValue: number;
+	value: number;
 	disabled: boolean;
 	children: React.ReactNode;
 	onMouseDown: (e: React.MouseEvent) => void;
